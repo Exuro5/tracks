ad4f9abeb630f0212a327e63c5568a6aa7f2ce3b
Tom <tomjoliver@outlook.com>
2021-03-31 18:52:51 +0100

    Authentication Flow

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import { NavigationEvents } from 'react-navigation';
+import AuthenticationForm from '../components/AuthenticationForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+
+    const { state, signin, clearErrorMessage } = useContext(AuthContext);
+
+
+    return (
+        <View style={styles.container}>
+            <NavigationEvents 
+                onDidBlur={clearErrorMessage}
+            />
+            <AuthenticationForm 
+                formHeader='Sign In for Tracker' 
+                errorMessage={state.errorMessage}
+                onSubmit={signin}
+                submitButtonText='Sign In'
+            /> 
+
+            <NavLink routeName='Signup' text="Don't have an account? Sign up here." />
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingBottom: 100
+        
+    },
+
+});
 
 export default SigninScreen;
